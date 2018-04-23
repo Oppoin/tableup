c2dd7157fa2d2195fbb87965c1f231774a57585f
Alexander Kozhurkin <kauker01@yandex.ru>
2018-04-23 21:32:37 +0300

    added configurable displaying options

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import EnhancedTable from './components/dataTable';
+import tableConfig from './config/tableConfig';
 
 class App extends Component {
   render() {
     return (
-      <EnhancedTable />
+      <EnhancedTable options={tableConfig} />
     );
   }
 }
---
+++ src/config/tableConfig.js
@@ -0,0 +1,9 @@
+const config = {
+    paging: false, 
+    rowsPerPageOptions: [2, 6, 10],
+    rowsPerPage: 5,
+    checkbox: false,
+    search: true
+}
+
+export default config;
