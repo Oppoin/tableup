d052ceadb436c33dc34f3e49c5282925691d44b4
Alexander Kozhurkin <kauker01@yandex.ru>
2018-04-25 16:32:44 +0300

    table options retrieved from outer scope

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
 import EnhancedTable from './components/dataTable';
-import tableConfig from './config/tableConfig';
 
 class App extends Component {
   render() {
     return (
-      <EnhancedTable options={tableConfig} />
+      <EnhancedTable options={window.tableConfig} />
     );
   }
 }
