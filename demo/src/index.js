2e1442d7caf7e5722ed64a64b708c0dad18abcc1
Patrik Toma <patotoma@gmail.com>
2018-05-12 23:05:12 +0700

    update demo app to suite latest API changes

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,14 +1,11 @@
 import React, {Component} from 'react';
 import {render} from 'react-dom';
-// import $ from 'jquery';
+
+import {handleFetch} from './services/api.js';
+import {apiUrl} from './constants.js';
 
 import TableUp from '../../src';
 import '../../css/tableup.css';
-
-import {handleFetch} from './services/api.js';
-
-// http://apiplaceholder.oppoin.com/users/?filter{username.icontains}=an&page=2&per_page=2
-const apiUrl = 'http://apiplaceholder.oppoin.com/users/';
 
 class Demo extends Component {
   state = {
@@ -22,24 +19,30 @@ class Demo extends Component {
 
   componentDidMount() {
     this.loadData();
-
-    // $.ajax({
-    //   url: apiUrl,
-    //   contentType: 'application/vnd.api+json'
-    // })
-    //   .done(function( data ) {
-    //     console.log('data', data);
-    //   });
   }
 
   loadData = () => {
-    handleFetch(`${apiUrl}?filter{username.icontains}=${this.state.filter.username}&page=${this.state.page}&per_page=${this.state.perPage}`,
-    {},
-    response => {
-      this.setState({
-        data: response.data,
-      });
-    });
+    let url = apiUrl;
+    // filter
+    if (this.state.filter.username) {
+      url = `${url}?filter{username.icontains}=${this.state.filter.username}&`;
+    } else {
+      url = `${url}?`;
+    }
+    // page
+    url = `${url}page=${this.state.page}&`;
+    // perPage
+    url = `${url}per_page=${this.state.perPage}`;
+
+    handleFetch(
+      url,
+      {},
+      response => {
+        this.setState({
+          data: response.data,
+        });
+      }
+    );
   }
 
   handleQuerySeach = query => {
@@ -57,7 +60,7 @@ class Demo extends Component {
     return (
       <div>
         <TableUp
-          data={this.state.users}
+          dataArray={this.state.data}
           onQuerySeach={this.handleQuerySeach}
           querySearchDebounceTime={200}
           querySearchHintText="Search"
