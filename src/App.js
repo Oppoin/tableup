5fd8fc358025318dff843f9f080367136b49f7ea
Alexander Kozhurkin <kauker01@yandex.ru>
2018-05-14 02:16:03 +0300

    configurable theme colors

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import EnhancedTable from './components/dataTable';
+import themeConfig from './config/themeConfig';
+import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
+
+const theme = createMuiTheme(themeConfig);
 
 class App extends Component {
   render() {
     return (
-      <EnhancedTable options={window.tableConfig} />
+      <MuiThemeProvider theme={theme}>
+        <EnhancedTable options={window.tableConfig} />
+      </MuiThemeProvider>
     );
   }
 }
