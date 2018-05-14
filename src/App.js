c01348cb13a9a3d8a3c57ff8b92d46e7659bfd89
Alexander Kozhurkin <kauker01@yandex.ru>
2018-05-14 11:42:09 +0300

    using themeConfig from global variable

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 import EnhancedTable from './components/dataTable';
-import themeConfig from './config/themeConfig';
 import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
 
-const theme = createMuiTheme(themeConfig);
+const theme = createMuiTheme(window.themeConfig);
 
 class App extends Component {
   render() {
